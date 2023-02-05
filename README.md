78c3cb43c1b2ee020115e6da23bbf80bb1baee87
onur1 <onur1@onurgunduz.com>
2023-02-05 06:49:45 +0100

    Add example

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # toposort
 
 sort a graph topologically.
+
+## Example
+
+```golang
+relations := map[string]string{
+  "Barbara": "Nick",
+  "Nick":    "Sophie",
+  "Sophie":  "Jonas",
+}
+
+sorted, _ := toposort.Sort(relations)
+
+fmt.Println(sorted)
+
+relations = map[string]string{
+  "Jonas": "Jonas",
+}
+
+_, err := toposort.Sort(relations)
+
+fmt.Println(err.Error())
+```
+
+Output:
+
+```
+[Jonas Sophie Nick Barbara]
+cyclic: [Jonas Jonas]
+```
---
+++ example_test.go
@@ -0,0 +1,31 @@
+package toposort_test
+
+import (
+	"fmt"
+
+	"github.com/onur1/toposort"
+)
+
+func ExampleSort() {
+	relations := map[string]string{
+		"Barbara": "Nick",
+		"Nick":    "Sophie",
+		"Sophie":  "Jonas",
+	}
+
+	sorted, _ := toposort.Sort(relations)
+
+	fmt.Println(sorted)
+
+	relations = map[string]string{
+		"Jonas": "Jonas",
+	}
+
+	_, err := toposort.Sort(relations)
+
+	fmt.Println(err.Error())
+
+	// Output:
+	// [Jonas Sophie Nick Barbara]
+	// cyclic: [Jonas Jonas]
+}
